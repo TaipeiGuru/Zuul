7ac6e1b01c36354ef9a8ff90b1c7b0ced4dc6c24
TaipeiGuru <78506727+TaipeiGuru@users.noreply.github.com>
2022-12-08 11:35:07 -0800

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+#include <cstring>
+#include "room.h"
+
+using namespace std;
+
+Room::Room() {
+  
+}
+
+/* setExits() {
+ return exits; 
+} */
+
+char* setDescription() {
+   return description;
+}
+
+Item* getItems() {
+  return item; 
+}
