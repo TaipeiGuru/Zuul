b12a03e70240222862613c7c720053deb7d7b0f7
TaipeiGuru <78506727+TaipeiGuru@users.noreply.github.com>
2022-12-09 14:11:19 -0800

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <iterator>
+#include <map>
 #include "room.h"
 
 using namespace std;
 
-Room::Room() {
+room::room() {
   
 }
 
-/* setExits() {
- return exits; 
-} */
+void room::listExits() {
+  map<char*, room*>::iterator iter;
+  for(iter = exits.begin(); iter != exits.end(); iter++) {
+    cout << it->first << endl; 
+  }
+} 
 
-char* setDescription() {
-   return description;
+char* room::showDescription() {
+  return description;
 }
 
-Item* getItems() {
-  return item; 
+vector <item*>* room::getItems() {
+  return &items; 
 }
+
+void room::dropItems(item* myItem) {
+  items.push_back(myItem);
+}
+
+void room::retrieveItems(item* myItem) {
+  vector<item*>::iterator iter;
+  for(iter = items.begin(); iter != items.end(); iter++) {
+    if(strcmp((*iter)->getItemType(), myItem->getItemType()) == 0) {
+      items.erase(iter);
+      return;
+    }
+  }
+}
+
+vector<item*>* 
+
+void room::
