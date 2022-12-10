94d5107bf7301201bab4589e7a6655d383d66d56
TaipeiGuru <78506727+TaipeiGuru@users.noreply.github.com>
2022-12-09 19:57:53 -0800

    Update item.h

--- item.h
+++ item.h
@@ -1,4 +1,17 @@
 #include <iostream>
 #include <cstring>
 
+#infdef ITEM_H
+#define ITEM_H
+
 using namespace std;
+
+class item {
+ public: 
+  item();
+  char* getItemType();
+ private:
+  char itemType[50];
+};
+
+#endif
